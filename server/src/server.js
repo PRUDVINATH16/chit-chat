d2882b5214b72d9b326697605f036cf13fa5ab49
prudvi <patnamprudvinath2006@gmail.com>
2025-10-01 17:53:43 +0530

    added profilePic route

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -16,6 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client")));
 
 // API routes
