118824bf548b784717cbf9e9c07910a5ca914f19
prudvi <patnamprudvinath2006@gmail.com>
2025-10-03 19:52:08 +0530

    Message Route Completed

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from "../controllers/message.controller.js";
+
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send message Endpoint!");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+
+router.post("/send/:id", sendMessage);
 
 export default router;
