467f3c85150bc132f0af86e791b5124852b0c4df
prudvi <patnamprudvinath2006@gmail.com>
2025-10-07 11:43:21 +0530

    Login Css Frontend Almost Completed

--- client/scripts/signup.js
+++ client/scripts/signup.js
@@ -1,15 +1,16 @@
+import doFetch from "./do_fetch.js";
+
 async function checkUser() {
   console.log("Checking user signup...");
-  const response = await fetch("http://localhost:3000/api/auth/signup", {
+  /* const response = await fetch("http://localhost:3000/api/auth/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ fullName: "Nagur Lalush", email: "nagurlalush@gmail.com", password: "password123"}),
-  });
+    body: JSON.stringify({ fullName: "Nagur Lalush", email: "nagurlalush@gmail.com", password: "password123"}), 
+  }); */
 
-  const data = await response.json();
-  console.log(data);
+  const req = await doFetch("auth/signup", "POST", { fullName: "Nagur Lalush", email: "nagurlalush@gmail.com", password: "password123"});
 }
 
-checkUser();
+//checkUser();
